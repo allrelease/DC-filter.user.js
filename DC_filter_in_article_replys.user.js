1f378d0c26d67ca1188a4e49766e0decdcaf47b3
allrelease <allrelease@gmail.com>
2018-10-13 11:29:53 +0900

    reply filter bug fixed : deleted, dc con reply exception

--- DC_filter_in_article_replys.user.js
+++ DC_filter_in_article_replys.user.js
@@ -5,7 +5,7 @@
 // @include     http://gall.dcinside.com/board/comment_view/*
 // @include     http://gall.dcinside.com/mgallery/board/view/*
 // @include     http://gall.dcinside.com/board/comment/*
-// @version     0.3.1
+// @version     0.3.2
 // @grant       none
 // @description   filter trolls by replys
 // @contributor   dot
@@ -37,53 +37,47 @@ function checkIP(target){
 
 //리플 토글시킴
 function toggleReply() {
-    var replyer = this.parentNode.getElementsByClassName('user user_layer') [0];
+    var replyer = this.parentNode.parentNode.parentNode.querySelector('.gall_writer, .ub-writer');
     var reply = this;
     // replyer.innerHTML = unescape(replyer.getAttribute('hideit'));
     replyer.innerHTML = replyer.getAttribute('hideit');
     //reply.innerHTML = unescape(reply.getAttribute('hideit'));
     reply.innerHTML = reply.getAttribute('hideit');
-    replyer.setAttribute('hideit', 'release');
-    // replyer.removeAttribute('hideit');
+    //replyer.setAttribute('hideit', 'release');
+    replyer.removeAttribute('hideit');
     reply.removeAttribute('hideit');
     this.removeEventListener('click', toggleReply);
 }
 
 //리플 지우기와 가리기
 function removeReply() {
-    //var reply_context = document.getElementsByClassName('view_comment') [0];
-    //var reply_table = reply_context.getElementsByTagName('cmt_list') [0];
+
     var replys = document.getElementsByClassName('ub-content');
-    
+
     for (var c = 0; c < replys.length; c++) {
         var each_line = replys[c];
-        var name_zone = each_line.querySelector('.gall_writer, .ub-writer');
-        // var name_zone = each_line.children[0];
+
+        if (each_line.getElementsByClassName('del_reply').length) continue; //삭제된 리플에서 건너뛰기
+
+        var name_zone = each_line.querySelector('.gall_writer.ub-writer');
+
         var user_name = name_zone.getAttribute('data-nick');
         var user_id = name_zone.getAttribute('data-uid');
-        var reply = each_line.querySelector(".usertxt, .ub-word");
+        var reply = each_line.querySelector(".usertxt.ub-word");
+        if(!reply) reply = each_line.querySelector("div.comment_dccon.clear"); // 디씨콘 처리
 
         var IP = name_zone.getAttribute('data-ip');
-        /*if(reply.getElementsByClassName('etc_ip').length != 0)
-        {
-            IP = reply.getElementsByClassName('etc_ip')[0].textContent;       
-        }*/
 
-        
-        // var reply = each_line.children[1];
-        
         //보지도 않을 넘을 지운다.
         //예외처리 먼저
         if (!name_zone.hasAttribute('hideit')) {
 	        if (!filter_id_exception.test(user_id) && !filter_name_exception.test(user_name)) {
 		        if (filter_name_del.test(user_name) || filter_id_del.test(user_id) || checkIP(IP)) {
-                    name_zone.setAttribute('hideit', 'B');
                     //name_zone.style.visibility = 'hidden';
                     name_zone.textContent='';
-                    // reply.setAttribute('hideit', 'a');
                     //reply.style.visibility = 'hidden';
                     reply.textContent='';
-		        } 
+		        }
                 //가리기만 할 넘들
 		        else if (filter_name.test(user_name) || filter_id.test(user_id)) {
                     //name_zone.setAttribute('hideit', escape(name_zone.innerHTML));
@@ -93,6 +87,8 @@ function removeReply() {
                     reply.setAttribute('hideit', reply.innerHTML);
                     reply.innerHTML = '<b>차단되었습니다</b>';
                     reply.addEventListener('click', toggleReply);
+                    //reply.querySelector('nickname').removeEventListener('click',get_user)
+                    //reply.addEventListener('mouseover', toggleReply);
 		        }
                 //초성닉은 사유없이 가린다
 		        else if ( filter_name_chosung.test(user_name) ) {
@@ -103,6 +99,7 @@ function removeReply() {
                     reply.setAttribute('hideit', reply.innerHTML);
                     reply.innerHTML = '<b>초성닉</b>';
                     reply.addEventListener('click', toggleReply);
+                    //reply.addEventListener('mouseover', toggleReply);
 		        }
 		    }
 		}
@@ -124,7 +121,7 @@ function throttle( fn, time ) {
 }
 // 리플 차단
 //var reply_div = document.getElementById('comment_list');
-var reply_div = document.querySelector('.comment_wrap, .show');
+var reply_div = document.querySelector('.comment_wrap.show');
 //reply_div.addEventListener('DOMNodeInserted', removeReply, true);
 reply_div.addEventListener('load', throttle(removeReply,10), true);
 //reply_div.addEventListener('DOMNodeInserted', throttle(removeReply,20), true);
