aa4a47b824eb4809bff9e903048fe6533c12d45c
allrelease <allrelease@gmail.com>
2019-02-07 00:36:17 +0900

    Merge branch 'master' of https://github.com/allrelease/DC-filter.user.js
    # Conflicts:
    #	DC_filter_in_article_replys.user.js
    #	DC_filter_in_board.user.js

--- DC_filter_in_article_replys.user.js
+++ DC_filter_in_article_replys.user.js
@@ -5,13 +5,13 @@
 // @include     http://gall.dcinside.com/board/comment_view/*
 // @include     http://gall.dcinside.com/mgallery/board/view/*
 // @include     http://gall.dcinside.com/board/comment/*
-// @version     20190126
+// @version     20190127
 // @grant       none
 // @description   filter trolls by replys
 // @contributor   dot
 // @updateURL https://github.com/allrelease/DC-filter.user.js/raw/master/DC_filter_in_article_replys.user.js
+// @run-at document-start
 // ==/UserScript==
-
 var filter_name = /^(?:두정갑|경번갑|ksy|근세돌|철구|숯불형인간)$/
 var filter_name_del = /(?:ㅡ|[asdㅁㄴㅇ]{6}|^(.)\1{1,2}|ㅄ|ㅂㅅ|악개.*|카지노|포항의봄|ㅇㄱㄹㅇㅂㅂㅂㄱ|ㅇㄱㄹㅇㅂㅂㅂ ㄱ|장소삼|irene|오타쿠|위키세계어|김용팔|에로망가센세|프로외노자|bluepick|야옹.*|행성.*|Foundation|통암기공부법)/
 var filter_name_chosung = /^(?:[-ㅁㅂㅈㄷㄳㅁㄴㅇㄹㅎㅅㅋㅌㅊㅍㄻㅀㅓㅗqwerasdfzxcvASDFㄱ01234]{1,10}|(.)\1{1,}|[,\.]{1,})$/
@@ -23,12 +23,13 @@ var filter_id_exception = /^(?:gaegogizzang)$/
 
 var filter_IP = ["1.11.*.*"]
 
+var run_once = 1;
 //var reg_reply = /td class="user user_layer" user_id="(.*?)" user_name="(.+?)"/g
 
 //아이피 확인
-function checkIP(target){
-    for(var count = 0; count < filter_IP.length;count++){
-        if(target === filter_IP[count]){
+function checkIP(target) {
+    for (var count = 0; count < filter_IP.length; count++) {
+        if (target === filter_IP[count]) {
             return true
         }
     }
@@ -51,7 +52,7 @@ function toggleReply() {
 }
 
 //리플 지우기와 가리기
-function removeReply() {
+function _removeReply() {
 
     var replys = document.getElementsByClassName('ub-content');
 
@@ -65,22 +66,22 @@ function removeReply() {
         var user_name = name_zone.getAttribute('data-nick');
         var user_id = name_zone.getAttribute('data-uid');
         var reply = each_line.querySelector(".usertxt.ub-word");
-        if(!reply) reply = each_line.querySelector("div.comment_dccon.clear"); // 디씨콘 처리
+        if (!reply) reply = each_line.querySelector("div.comment_dccon.clear"); // 디씨콘 처리
 
         var IP = name_zone.getAttribute('data-ip');
 
         //보지도 않을 넘을 지운다.
         //예외처리 먼저
         if (!name_zone.hasAttribute('hideit')) {
-	        if (!filter_id_exception.test(user_id) && !filter_name_exception.test(user_name)) {
-		        if (filter_name_del.test(user_name) || filter_id_del.test(user_id) || checkIP(IP)) {
+            if (!filter_id_exception.test(user_id) && !filter_name_exception.test(user_name)) {
+                if (filter_name_del.test(user_name) || filter_id_del.test(user_id) || checkIP(IP)) {
                     //name_zone.style.visibility = 'hidden';
-                    name_zone.textContent='';
+                    name_zone.textContent = '';
                     //reply.style.visibility = 'hidden';
-                    reply.textContent='';
-		        }
+                    reply.textContent = '';
+                }
                 //가리기만 할 넘들
-		        else if (filter_name.test(user_name) || filter_id.test(user_id)) {
+                else if (filter_name.test(user_name) || filter_id.test(user_id)) {
                     //name_zone.setAttribute('hideit', escape(name_zone.innerHTML));
                     name_zone.setAttribute('hideit', name_zone.innerHTML);
                     name_zone.innerHTML = '<b>차단</b>';
@@ -90,9 +91,9 @@ function removeReply() {
                     reply.addEventListener('click', toggleReply);
                     //reply.querySelector('nickname').removeEventListener('click',get_user)
                     //reply.addEventListener('mouseover', toggleReply);
-		        }
+                }
                 //초성닉은 사유없이 가린다
-		        else if ( filter_name_chosung.test(user_name) ) {
+                else if (filter_name_chosung.test(user_name)) {
                     //name_zone.setAttribute('hideit', escape(name_zone.innerHTML));
                     name_zone.setAttribute('hideit', name_zone.innerHTML);
                     name_zone.innerHTML = '<b>초성닉</b>';
@@ -101,28 +102,51 @@ function removeReply() {
                     reply.innerHTML = '<b>초성닉</b>';
                     reply.addEventListener('click', toggleReply);
                     //reply.addEventListener('mouseover', toggleReply);
-		        }
-		    }
-		}
+                }
+            }
+        }
     }
 }
 
-function throttle( fn, time ) {
+function throttle(fn, time) {
     var t = 0;
     return function() {
         var args = arguments,
             ctx = this;
 
-            clearTimeout(t);
+        clearTimeout(t);
 
-        t = setTimeout( function() {
-            fn.apply( ctx, args );
-        }, time );
+        t = setTimeout(function() {
+            fn.apply(ctx, args);
+        }, time);
     };
 }
+
+function removeReply() {
+    if (run_once) {
+        run_once = 0;
+        var once = new throttle(function() {
+            run_once = 1;
+        }, 100);
+        once();
+        _removeReply();
+    }
+}
+
+function Invoker() {
+    var reply_div = document.getElementsByClassName('comment_wrap')[0];
+    if (reply_div) {
+        //reply_div.addEventListener('load', throttle(removeReply,0), true);
+        reply_div.addEventListener('load', removeReply, true);
+        //reply_div.addEventListener('DOMNodeInserted', removeReply);
+        document.removeEventListener('load', Invoker, true);
+    }
+}
+
 // 리플 차단
-var reply_div = document.getElementsByClassName('comment_wrap')[0];
+//var reply_div = document.getElementsByClassName('comment_wrap')[0];
 //var reply_div = document.querySelector('.comment_wrap.show');
 //reply_div.addEventListener('DOMNodeInserted', removeReply, true);
-reply_div.addEventListener('load', throttle(removeReply,10), true);
-//reply_div.addEventListener('DOMNodeInserted', throttle(removeReply,20), true);
+//reply_div.addEventListener('load', throttle(removeReply,500), true);
+//document.addEventListener('DOMNodeInserted', function(){var reply_div = document.getElementsByClassName('comment_wrap')[0];removeReply();}, true);
+document.addEventListener('load', Invoker, true);
